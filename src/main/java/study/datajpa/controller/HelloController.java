a2f6fd166530ca82797466ad13bea7fae5935f8f
Kim9xd <kim9xd@gmail.com>
2021-12-17 14:18:33 +0900

    * 예제 도메인 모델   - 예제 도메인 모델과 동작확인
      * Update gitignore
      * chagne gradle to maven project

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import study.datajpa.entity.Team;
 
 @Slf4j
 @RestController

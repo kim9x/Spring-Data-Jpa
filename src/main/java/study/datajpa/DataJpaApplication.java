50d00f883c54b37ecc51d1e31e59d650738742f9
Kim9xd <kim9xd@gmail.com>
2021-12-22 00:22:11 +0900

    * 확장 기능   - Auditing

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,13 +1,25 @@
 package study.datajpa;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // (modifyOnCreate = false) update는 null 값이 들어감 그러나 사용하지 않을듯?
 @SpringBootApplication
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
+	}
+	
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());  
 	}
 
 }
--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -8,9 +8,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import study.datajpa.repository.MemberRepository;
 
 @SpringBootTest
 @Transactional
@@ -19,6 +22,9 @@ class MemberTest {
 
 	@PersistenceContext
 	EntityManager em;
+	
+	@Autowired
+	MemberRepository memberRepository;
 	
 	@Test
 	public void testEntity() {
@@ -48,6 +54,30 @@ class MemberTest {
 			System.out.println("-> member.team = " + member.getTeam());
 		}
 		
+	}
+	
+	@Test
+	public void JpaEventBaseEntity() throws Exception {
+		// given
+		Member member = new Member("member1");
+		memberRepository.save(member); // @PrePersist가 발생함.
+		
+		Thread.sleep(100);
+		
+		member.setUsername("member2");
+		
+		em.flush();
+		em.clear();
+		
+		// when
+		Member findMember = memberRepository.findById(member.getId()).get();
+		
+		// then
+		System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+//		System.out.println("findMember.updatedDate = " + findMember.getUpdatedDate());
+		System.out.println("findMember.updatedDate = " + findMember.getLastModifiedDate());
+		System.out.println("findMember.createdBy = " + findMember.getCreatedBy());
+		System.out.println("findMember.lastModifiedBy = " + findMember.getLastModifiedBy());
 	}
 
 }

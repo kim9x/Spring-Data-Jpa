a249ce4f37da6b616b30878e59cadb8c8d3a061a
Kim9xd <kim9xd@gmail.com>
2021-12-22 12:56:49 +0900

    * 확장 기능   - Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,6 +2,9 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
+import study.datajpa.repository.dto.MemberDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,9 +37,22 @@ public class MemberController {
 		return member.getUsername();
 	}
 	
+	@GetMapping("/members")
+	public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable ) {
+		/*
+		 * Page<Member> page = memberRepository.findAll(pageable); Page<MemberDto> map =
+		 * page.map(member -> new MemberDto(member.getId(), member.getUsername(),
+		 * null)); return map;
+		 */
+		return memberRepository.findAll(pageable)
+				.map(MemberDto::new);
+	}
+	
 	@PostConstruct
 	public void init() {
-		memberRepository.save(new Member("userA"));
+		for (int i = 0; i < 100; i++) {
+			memberRepository.save(new Member("user" + i, i));
+		}
 	}
 
 }
--- src/main/java/study/datajpa/repository/dto/MemberDto.java
+++ src/main/java/study/datajpa/repository/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.repository.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -13,6 +14,14 @@ public class MemberDto {
 		this.id = id;
 		this.username = username;
 		this.teamName = teamName;
+	}
+	
+	// DTO를 쉽게 생성하여 return하는 방법
+	// 아래 방식으로 사용하면 method reference 방식을 사용하여
+	// 코드가 훨씬 짧아질 수 있다.
+	public MemberDto(Member member) {
+		this.id = member.getId();
+		this.username = member.getUsername();
 	}
 
 }

f9ca43add8f46b1e4d4c16f427262d4d615e0148
Kim9xd <kim9xd@gmail.com>
2021-12-22 14:40:31 +0900

    * 확장 기능   - 스프링 데이터 JPA 구현체 분석   - 새로운 엔티티를 구별하는 방법

---
+++ src/test/java/study/datajpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,28 @@
+package study.datajpa.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import study.datajpa.entity.Item;
+
+@SpringBootTest
+class ItemRepositoryTest {
+	
+	@Autowired ItemRepository itemRepository;
+
+	@Test
+	public void save() {
+		
+		Item item = new Item();
+		
+		// item의 id 값이 null 값이 들어간다.
+		// persist 할 때 id 값 생성.!
+		// 여기서 말하는 id 값이란 Entity 부분에서
+		// @id annotation이 지정된 것을 말한다.
+		itemRepository.save(item);
+	}
+
+}
